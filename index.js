37c6ceabf7a8d9f40b2bae8354609b6318b9a49c
Sergey Novikov <snovikov196605@gmail.com>
2022-10-18 22:08:04 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,16 +9,41 @@
 // foo();
 // console.log("Counter is " + count);
 
-function foo(start) {
+// function foo(start) {
   
-  let stopInterval = setInterval(() => {
-    start++;
-    if (start ===60) {
-      clearInterval(stopInterval);
-    }
-    console.log('Hello, World!', start);
-  },1000);
+//   let stopInterval = setInterval(() => {
+//     start++;
+//     if (start ===60) {
+//       clearInterval(stopInterval);
+//     }
+//     console.log('Hello, World!', start);
+//   },1000);
 
+// }
+
+// foo(0);
+
+function myClock() {
+      setInterval(function() {
+      var date = new Date();
+
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var second = date.getSeconds();
+
+      if(hours <10) {
+        hours = "0" + hours;
+      }
+      if(minutes <10) {
+        minutes = "0" + minutes;
+      }
+      if(second <10) {
+        second = "0" + second;
+      }
+
+      var clock = hours + ":" + minutes + ":" + second;
+      
+      console.log(clock);
+   }, 1000);
 }
-
-foo(0);
+myClock();
